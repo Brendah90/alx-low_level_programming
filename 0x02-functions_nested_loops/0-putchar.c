c4f2f1e8d53bc59a202dcef8da41217439d60593
Brendah90 <bregamah@gail.com>
2023-06-21 12:22:21 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,6 @@
  * is written by mbah
  * Return: 0
  */
-
 int main(void)
 {
 _putchar('_');
@@ -16,5 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return(0);
+return (0);
 }
